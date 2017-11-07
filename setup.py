1dc44586c9e728690c69f5ac4148c594eeb343ba
Marek Suchánek <marek.suchanek@gmail.com>
2017-11-07 14:17:59 +0100

    Allow simple installation with setup.py
    I created setup.py for simple installation and distribution. LICENSE file and example config need to be included with MANIFEST.in. Old requirements were replaced with full pip freeze output (verified env).

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='labelord_suchama4',
+    version='0.3',
+    keywords='github labels management replication cli web',
+    description='Simple CLI and WEB tools for managing GitHub labels',
+    long_description=long_description,
+    author='Marek Suchánek',
+    author_email='suchama4@fit.cvut.cz',
+    license='MIT',
+    url='https://github.com/MarekSuchanek/labelord',
+    zip_safe=False,
+    packages=find_packages(),
+    package_data={
+        'labelord': [
+            'static/*.css',
+            'templates/*.html',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'labelord = labelord:cli',
+        ]
+    },
+    install_requires=[
+        'click',
+        'Flask',
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
